aef55fe7b5982eb71d9f764cb16262e407158717
Roxana Zahan <c_rzahan@groupon.com>
2020-01-09 12:33:15 +0200

    Did initial setup and added Primitives class

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Primitives primitives = new Primitives();
+
+        System.out.println(primitives.convertToCelsius(47));
+
+    }
+}
---
+++ src/main/java/Primitives.java
@@ -0,0 +1,17 @@
+public class Primitives {
+
+        //Practice with byte, short, long and integers - this only returns integers
+    public int convertToCelsius(int fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+        //Practice with float and double
+            //Write a Java program that reads a number in inches, converts it to meters.
+            //Note: One inch is 0.0254 meter.
+
+        //Practice with char
+
+        //Practice with booleans
+
+
+}
