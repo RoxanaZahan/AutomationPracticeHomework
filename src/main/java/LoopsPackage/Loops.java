ca5ea6d4e6e1eea869b7f962d96e329045c4bed8
Roxana Zahan <c_rzahan@groupon.com>
2020-02-06 14:25:26 +0200

    Added Bricks exercise in Loops Class and an extra method for practicing with Arrays for Exercise 1

--- src/main/java/LoopsPackage/Loops.java
+++ src/main/java/LoopsPackage/Loops.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 
 public class Loops {
 
+    //https://codingbat.com/prob/p159339
 //    Return an array that contains exactly the same numbers as the given array, but rearranged so that every 3 is
 //    immediately followed by a 4. Do not move the 3's, but every other number may move.
 //    The array contains the same number of 3's and 4's, every 3 has a number after it that is not a 3, and a 3 appears
@@ -13,6 +14,25 @@ public class Loops {
 //    fix34([1, 3, 1, 4, 4, 3, 1]) → [1, 3, 4, 1, 1, 3, 4]
 //    fix34([3, 2, 2, 4]) → [3, 4, 2, 2]
 
+    //using Arrays - this can be tested in codingbat
+    public int[] getMutantArray(int[] myArray) {
+        for(int i = 0; i< myArray.length; i++) {
+            if(myArray[i] == 3 && myArray[i+1] != 4) {
+                for(int e = 0; e < myArray.length; e++) {
+                    if (myArray[e] == 4 && myArray[e-1] != 3) {
+                        int toMove = myArray[i+1];
+                        int index = e;
+                        myArray[i+1] = myArray[e];
+                        myArray[index] = toMove;
+                        break;
+                    }
+                }
+            }
+        }
+        return myArray;
+    }
+
+    //using ArrayList
     public ArrayList getMutantList1(ArrayList<Integer> myList) {
         for(int i = 0; i < myList.size(); i++) {
             if(myList.get(i) == 3) {
@@ -30,7 +50,7 @@ public class Loops {
         return myList;
     }
 
-    //Use Collections
+    //Using Collections
     public ArrayList getMutantList2(ArrayList<Integer> myList) {
         for(int i = 0; i < myList.size(); i++) {
             if(myList.get(i) == 3) {
@@ -59,5 +79,46 @@ public class Loops {
             }
         }
         return myIntArray;
+    }
+
+    //https://codingbat.com/prob/p183562
+//    We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) and big bricks (5 inches each).
+//    Return true if it is possible to make the goal by choosing from the given bricks.
+//    makeBricks(3, 1, 8) → true
+//    makeBricks(3, 1, 9) → false
+//    makeBricks(3, 2, 10) → true
+
+    //without loops
+    public boolean makeBricks(int small, int big, int goal) {
+        int wall = 0;
+        if(goal % 5 >= 0 && big != 0 && goal/5 >= big) {
+            wall += big*5;
+        } else if(goal % 5 >= 0 && big != 0) {
+            wall += (goal / 5) * 5;
+        }
+        if(wall<goal && goal-wall <= small) {
+            wall += (goal-wall) * 1;
+        }
+        return wall == goal;
+    }
+
+    //with loops
+    public boolean makeBricks2(int small, int big, int goal) {
+        int wall = 0;
+        while(wall < goal && big > 0) {
+            wall += 5;
+            big --;
+            if (wall+5>goal) {
+                break;
+            }
+        }
+        while(wall < goal && small > 0) {
+            wall += 1;
+            small --;
+            if (wall+1>goal) {
+                break;
+            }
+        }
+        return wall == goal;
     }
 }
--- src/main/java/LoopsPackage/Main.java
+++ src/main/java/LoopsPackage/Main.java
@@ -8,38 +8,39 @@ public class Main {
     public static void main(String[] args) {
 
         Loops loops = new Loops();
-        ArrayList<Integer> myNumbersList1 = new ArrayList<Integer>();
 
-        myNumbersList1.add(1);
-        myNumbersList1.add(3);
-        myNumbersList1.add(2);
-        myNumbersList1.add(4);
+        ArrayList<Integer> myNumbersList = new ArrayList<Integer>();
 
-        ArrayList<Integer> myNumbersList2 = new ArrayList<Integer>();
+        myNumbersList.add(1);
+        myNumbersList.add(3);
+        myNumbersList.add(2);
+        myNumbersList.add(4);
+        myNumbersList.add(3);
+        myNumbersList.add(5);
+        myNumbersList.add(4);
+        myNumbersList.add(1);
 
-        myNumbersList2.add(1);
-        myNumbersList2.add(3);
-        myNumbersList2.add(2);
-        myNumbersList2.add(4);
-        myNumbersList2.add(3);
-        myNumbersList2.add(5);
-        myNumbersList2.add(4);
-        myNumbersList2.add(1);
+        int[] myIntArray = {3, 2, 4, 3, 2, 4, 1};
 
         //    Return an array that contains exactly the same numbers as the given array, but rearranged so that every 3 is
         //    immediately followed by a 4. Do not move the 3's, but every other number may move.
         //    The array contains the same number of 3's and 4's, every 3 has a number after it that is not a 3, and a 3 appears
         //    in the array before any 4.
         System.out.println("\nExercise 1:");
-        System.out.println(loops.getMutantList1(myNumbersList1));
-        System.out.println(loops.getMutantList1(myNumbersList2));
+        System.out.println("\n- Using Arrays:");
+        System.out.println(Arrays.toString(loops.getMutantArray(myIntArray)));
+        System.out.println("\n- Using ArrayList:");
+        System.out.println(loops.getMutantList1(myNumbersList));
         System.out.println("\n- with Collections: ");
-        System.out.println(loops.getMutantList2(myNumbersList1));
-        System.out.println(loops.getMutantList2(myNumbersList2));
+        System.out.println(loops.getMutantList2(myNumbersList));
 
         // Write a Java program to replace every element with the next smallest element (from right side) in a given array of integers.
-        int[] myIntArray = {3, 2, 4, 3, 2, 4, 1};
         System.out.println("\nExercise 2:");
         System.out.println(Arrays.toString(loops.rearrangeArray(myIntArray)));
+
+        System.out.println("\nExercise 3:");
+        System.out.println("Can I build a wall with these bricks?");
+        System.out.println(loops.makeBricks(2,7,36));
+        System.out.println(loops.makeBricks2(5,0,12));
     }
 }
