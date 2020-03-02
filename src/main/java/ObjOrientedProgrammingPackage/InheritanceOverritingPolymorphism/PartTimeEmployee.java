f73cadbf96deac86e8aafcc0ba368a61a1c92a01
Roxana Zahan <c_rzahan@groupon.com>
2020-03-02 16:33:10 +0200

    Created subpackage InheritanceOverritingPolymorphism with the following classes: Main, Person, Employee and PartTimeEmployee - all for inheritance practice

---
+++ src/main/java/ObjOrientedProgrammingPackage/InheritanceOverritingPolymorphism/PartTimeEmployee.java
@@ -0,0 +1,14 @@
+package ObjOrientedProgrammingPackage.InheritanceOverritingPolymorphism;
+
+public class PartTimeEmployee extends Employee {
+    private double partSalary;
+
+    public PartTimeEmployee(String firstName, String lastName, String birthDate, double salary) {
+        super(firstName, lastName, birthDate, salary);
+    }
+
+    public double getSalary() {
+        this.partSalary = salary - (salary * 0.25);
+        return partSalary;
+    }
+}
